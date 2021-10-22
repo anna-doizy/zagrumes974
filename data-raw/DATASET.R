757007a568f9502aaee1dbc3df31f42dfaf882a8
anna-doizy <doizy.anna@gmail.com>
2021-10-22 10:42:15 +0400

    Improve dataset
    Make communes valid
    Put nicer labels in prelev
    Add randomness to the coordinates for preserving anonymity

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,16 +16,28 @@ devtools::document()
 library(dplyr)
 library(sf)
 
+communes <- read_sf("data-raw/communes.shp") %>% 
+  st_set_precision(1e6) %>% 
+  st_make_valid() # probleme au PORT (st_is_valid)
+
+pluvio <- read_sf("data-raw/pluvio.shp")
+
+
+set.seed(562)
+
 prelev <- readr::read_csv2("data-raw/data_agrumile.csv") %>% 
   filter(!is.na(Date)) %>% 
   select(-Type) %>% 
+  st_as_sf(coords=c('X', 'Y'), crs = st_crs(communes), remove = FALSE) %>% 
+  st_intersection(communes) %>%
+  as_tibble() %>% 
   mutate(
     Date = lubridate::dmy(Date),
-    Maladie = factor(Maladie)
+    Maladie = factor(Maladie, labels = c("Sain", "Malade")),
+    X = X + rnorm(n(), sd = 0.01),
+    Y = Y + rnorm(n(), sd = 0.01)
   )
 
-communes <- read_sf("data-raw/communes.shp")
-pluvio <- read_sf("data-raw/pluvio.shp")
 
 usethis::use_data(prelev, communes, pluvio, overwrite = TRUE)
 devtools::document() # modifier R/data.R
